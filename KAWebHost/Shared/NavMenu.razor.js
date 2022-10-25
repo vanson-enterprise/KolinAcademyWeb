38f3c2ace233d80d4541869769e9929eece78754
thuhuyen <nguyenthuhuyen@gmail.com>
2022-10-25 22:25:48 +0700

    fix ui home page

--- KAWebHost/Shared/NavMenu.razor.js
+++ KAWebHost/Shared/NavMenu.razor.js
@@ -1,7 +1,13 @@
 ﻿// toggle menu
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
+const namePage = document.querySelector(".name-page");
 
 navToggle.addEventListener("click", function () {
     links.classList.toggle("show-links");
+});
+
+// change text
+links.addEventListener("click", (e) => {
+    namePage.innerHTML = e.target.innerHTML;
 });
