2872f9bbcc62e1aba5db03ff586b5813d1fe0a3b
vanson <nguyenvanson98123@gmail.com>
2024-11-08 11:51:42 +0700

    news-page-ui-binding

--- KAWebHost/Pages/Site/News.razor.cs
+++ KAWebHost/Pages/Site/News.razor.cs
@@ -7,7 +7,7 @@ namespace KAWebHost.Pages.Site
     public partial class News : OwningComponentBase
     {
         private IBlogService _blogService;
-        private const int BLOG_AMOUNT_PER_PAGE = 4;
+        private const int BLOG_AMOUNT_PER_PAGE = 12;
         private int pageIndex = 1;
         private int blogsNumber = 0;
         private BlogSitePageVm model = new();
@@ -22,16 +22,18 @@ namespace KAWebHost.Pages.Site
             if (pageNumber == -1 && pageIndex > 1)
             {
                 pageIndex--;
+                GetModelData();
             }
             else if (pageNumber == 0 && pageIndex < BLOG_AMOUNT_PER_PAGE)
             {
                 pageIndex++;
+                GetModelData();
             }
-            else
+            else if(pageIndex != pageNumber && pageNumber != 0 && pageNumber != -1)
             {
                 pageIndex = pageNumber;
+                GetModelData();
             }
-            GetModelData();
         }
 
         private async Task GetModelData()
